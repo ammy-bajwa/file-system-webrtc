e7823d0e504ad8bb79f2d4d0f112426f336025b3
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-05 16:16:03 +0500

    Initialising socket

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import { useEffect } from "react";
+import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { alivaWS } from "./socket/index";
+import { render } from "@testing-library/react";
 
-function App() {
-  useEffect(() => {
-    alivaWS.initializeSocket("ws://localhost:4000/socket");
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount() {
+    await alivaWS.initializeSocket("ws://localhost:4000/socket");
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/socket/initialization/initializeSocket.js
+++ src/socket/initialization/initializeSocket.js
@@ -1,18 +1,25 @@
 import { Socket } from "phoenix";
 
-export const initializeSocket = (wsAddress) => {
-  const socket = new Socket(wsAddress);
+export const initializeSocket = function (wsAddress) {
+  return new Promise(function (resolve, reject) {
+    const socket = new Socket(wsAddress);
+    socket.connect();
 
-  socket.connect();
-
-  // Now that you are connected, you can join channels with a topic:
-  let channel = socket.channel("channel:signal", {});
-  channel
-    .join()
-    .receive("ok", (resp) => {
-      console.log("Joined successfully", resp);
-    })
-    .receive("error", (resp) => {
-      console.log("Unable to join", resp);
-    });
+    socket.onOpen = function () {
+      console.log("Socket is open");
+      this.socket = socket;
+    };
+    // Now that you are connected, you can join channels with a topic:
+    let channel = socket.channel("channel:signal", {});
+    channel
+      .join()
+      .receive("ok", (resp) => {
+        console.log("Joined successfully", resp);
+        resolve(socket);
+      })
+      .receive("error", (resp) => {
+        console.log("Unable to join", resp);
+        reject(resp);
+      });
+  });
 };
