57e36f32201959b3e928f5421bd7d526506703db
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-06 22:48:32 +0500

    Getting chunks array for files

--- src/fileUtils/getChunksArr/getChunksArr.js
+++ src/fileUtils/getChunksArr/getChunksArr.js
@@ -1,8 +1,26 @@
 export const getChunksArr = async (fileSize, chunkSize) => {
   const getChunksArrPromise = new Promise((resolve, reject) => {
-    const numberOfArrayChunks = Math.ceil(fileSize / chunkSize);
-    // chunkSize
-    resolve(numberOfArrayChunks);
+    const numberOfArrayChunks = Math.floor(fileSize / chunkSize);
+    const totalBaseChunksSize = numberOfArrayChunks * 40000;
+    const lastChunkSize = fileSize - totalBaseChunksSize;
+    let startSliceIndex = 0,
+      endSliceIndex = chunkSize,
+      chunksArr = [];
+    for (let index = 0; index < numberOfArrayChunks; index++) {
+      const chunkInfo = {
+        startSliceIndex,
+        endSliceIndex,
+      };
+      chunksArr.push(chunkInfo);
+      startSliceIndex = endSliceIndex;
+      endSliceIndex += chunkSize;
+    }
+    const lastChunkObj = {
+      startSliceIndex: endSliceIndex,
+      endSliceIndex: endSliceIndex + lastChunkSize,
+    };
+    chunksArr.push(lastChunkObj);
+    resolve(chunksArr);
   });
   return await getChunksArrPromise;
 };
