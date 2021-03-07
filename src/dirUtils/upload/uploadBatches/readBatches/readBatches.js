119642fd8cefd15eb27b4c2e4a46450b72bc1e77
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-07 14:59:45 +0500

    Update readBatches.js

--- src/dirUtils/upload/uploadBatches/readBatches/readBatches.js
+++ src/dirUtils/upload/uploadBatches/readBatches/readBatches.js
@@ -7,6 +7,7 @@ export const readBatches = async (file, batchesMetaData) => {
     if (Object.hasOwnProperty.call(batchesMetaData, batchKey)) {
       const { chunks } = batchesMetaData[batchKey];
       const batchWithFileChunks = await addFileDataToChunks(file, chunks);
+      // save batch to index db
       console.log("batchWithFileChunks: ", batchWithFileChunks);
     }
   }
