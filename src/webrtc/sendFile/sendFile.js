e69b50133c34ec71cd44b6657af46fae59608599
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-11 15:29:47 +0500

    Update sendFile.js

--- src/webrtc/sendFile/sendFile.js
+++ src/webrtc/sendFile/sendFile.js
@@ -14,7 +14,7 @@ export const sendFile = async (fileName) => {
   const batchesKeys = Object.keys(batchesMetadata);
   const currentDcCount = Object.keys(alivaWebRTC.dataChannels).length;
   if (currentDcCount < 4) {
-    await alivaWebRTC.settingUpDatachannels(400);
+    await alivaWebRTC.settingUpDatachannels(100);
   } else {
     console.log(`${currentDcCount} data channels already exists`);
   }
