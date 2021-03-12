7fd168b5cbcf97be87afeb9abd836caff0ddcba1
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-12 19:34:47 +0500

    Update checkIfAlreadyExist.js

--- src/idbUtils/checkIfAlreadyExist/checkIfAlreadyExist.js
+++ src/idbUtils/checkIfAlreadyExist/checkIfAlreadyExist.js
@@ -4,15 +4,17 @@ export const checkIfAlreadyExist = (dbName) => {
   return new Promise(async (resolve, reject) => {
     try {
       let isAlreadyExist = true;
-      const db = await openDB(dbName, 1, {
-        upgrade() {
+      const db = await openDB(dbName, 1);
+      const batchMetadata = await db.getAll("batchMetadata");
+      for (let index = 0; index < batchMetadata.length; index++) {
+        const { startSliceIndex, endSliceIndex } = batchMetadata[index];
+        const key = `${startSliceIndex}__${endSliceIndex}`;
+        const isChunkExist = await db.get("chunks", key);
+        if (!isChunkExist) {
           isAlreadyExist = false;
-        },
-      });
-      db.close();
-      if (!isAlreadyExist) {
-        await deleteDB(dbName);
+        }
       }
+      db.close();
       resolve(isAlreadyExist);
     } catch (error) {
       reject(error);
