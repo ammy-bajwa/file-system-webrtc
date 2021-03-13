cf652b1252e4dc7b7ece56ecf320b3d1cf8fe797
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-13 22:47:06 +0500

    Loading blob instead of chunk

--- src/idbUtils/loadBatchOfChunks/loadBatchOfChunks.js
+++ src/idbUtils/loadBatchOfChunks/loadBatchOfChunks.js
@@ -4,14 +4,16 @@ export const loadBatchOfChunks = async (batchHash, fileName, chunks) => {
   return new Promise(async (resolve, reject) => {
     try {
       const dbName = batchHash;
-      const storeName = "chunks";
+      const storeName = "blob";
       const db = await openDB(dbName, 1);
       const chunksKeys = Object.keys(chunks);
+      const storedBlob = await db.get(storeName, "data");
       let fileChunksFromIDB = {};
       for (let index = 0; index < chunksKeys.length; index++) {
         const chunkKey = chunksKeys[index];
-        const storedChunk = await db.get(storeName, chunkKey);
-        fileChunksFromIDB[chunkKey] = storedChunk;
+        const { startSliceIndex, endSliceIndex } = chunks[chunkKey];
+        const blockChunk = storedBlob.slice(startSliceIndex, endSliceIndex);
+        fileChunksFromIDB[chunkKey] = blockChunk;
       }
       db.close();
       resolve(fileChunksFromIDB);
