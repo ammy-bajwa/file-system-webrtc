21a80f4e826db0e81f95488131500b427545bf04
usamasarfraz <usamasarfraz822@gmail.com>
2021-03-24 12:40:33 +0500

    adding receive state

--- src/utils/manageRedux.js
+++ src/utils/manageRedux.js
@@ -1,7 +1,7 @@
 import store from '../redux/store';
 import { fileActions } from '../redux/actions/index';
 import { getAllSavedFiles } from "../idbUtils/getAllSavedFiles/getAllSavedFiles";
-const { storeMachineIDAndFiles, storeFile, moveFileToidbState, removeFileFromidbState } = fileActions;
+const { storeMachineIDAndFiles, storeFile, moveFileToidbState, removeFileFromidbState, saveMetadataInState } = fileActions;
 export default {
     storeState: function(data){
         store.dispatch(storeMachineIDAndFiles(data))
@@ -15,5 +15,8 @@ export default {
     },
     removeFileFromidbFiles: function(fileName){
         store.dispatch(removeFileFromidbState({fileName}))
+    },
+    saveReceivedMetadataInState: function(data){
+        store.dispatch(this.saveMetadataInState({data}))
     },
 }
