56eb70b110f4b25b445a51a8f32ef82efade7168
usamasarfraz <usamasarfraz822@gmail.com>
2021-03-26 16:29:22 +0500

    adding video player

--- src/components/displayIdbFiles.jsx
+++ src/components/displayIdbFiles.jsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+import { getFileBatchesFromIDB } from "../idbUtils/getFileBatches/getFileBatches";
+import Modal from "./modal";
 const DisplayIdbFiles = function ({ files }) {
+
+  const [modalIsOpen,setIsOpen] = useState(false);
+  const [file,setFile] = useState("");
+
+  const getVideo = async (batchesMetaData) => {
+    let getFile = await getFileBatchesFromIDB(batchesMetaData);
+    let fileURL = await URL.createObjectURL(getFile);
+    
+    setFile(fileURL);
+    openModal();
+  }
+  function openModal() {
+    setIsOpen(true);
+  }
+ 
+  function closeModal(){
+    setIsOpen(false);
+  }
   return (
     <div className="d-flex justify-content-center flex-wrap m-4">
       {files.length === 0 && <h3 className="text-info">No File</h3>}
@@ -9,7 +30,7 @@ const DisplayIdbFiles = function ({ files }) {
         ) => (
           <div key={i}>
             {!isReceived && !isOnlyMetadata && (
-              <span className="border border-dark rounded m-2 p-2">
+              <span className="border border-dark rounded m-2 p-3">
                 {name}--<b>{(size / 1000 / 1000).toFixed(2)}_MB</b>
                 {/* {isDelete ? (
               <button
@@ -20,8 +41,10 @@ const DisplayIdbFiles = function ({ files }) {
               Delete
               </button>
             ) : null} */}
+            <button className="btn btn-success m-1" onClick={()=>getVideo(batchesMetaData)}>Play</button>
               </span>
             )}
+            <Modal modalIsOpen={modalIsOpen} openModal={openModal} closeModal={closeModal} file={file} />
           </div>
         )
       )}
