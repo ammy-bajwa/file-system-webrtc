de4c5e3d9f876066bfe9b791c186fabab9485350
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-27 11:14:09 +0500

    adding logs

--- src/webrtc/sendFile/sendFile.js
+++ src/webrtc/sendFile/sendFile.js
@@ -32,65 +32,64 @@ export const sendFile = (fileName) => {
       setStatus("<h2>Setting up peerconnection and datachannels...</h2>");
 
       const isPcAlreadyExists = await isAlreadyConnected(fileName);
-
       if (!isPcAlreadyExists) {
         await alivaWebRTC.setupFilePeerConnection(fileName);
-        // Request other for to create peerconnection for file
-        await requestReceiverToSetupPC(fileName);
-        // After successfully creating peerconnection on receiver create on in sender
+        //   // Request other for to create peerconnection for file
+        //   await requestReceiverToSetupPC(fileName);
+        //   // After successfully creating peerconnection on receiver create on in sender
 
-        await alivaWebRTC.initializeFileDataChannels(fileName);
+        //   await alivaWebRTC.initializeFileDataChannels(fileName);
+        // }
+
+        // // const currentDcCount = Object.keys(alivaWebRTC.dataChannels).length;
+        // // console.log("batchesKeys: ", batchesKeys);
+        // // if (currentDcCount < 4) {
+        // //   await alivaWebRTC.settingUpDatachannels(400);
+        // // } else {
+        // //   console.log(`${currentDcCount} data channels already exists`);
+        // // }
+        // for (let key = 0; key < batchesKeys.length; key++) {
+        //   const batchKey = batchesKeys[key];
+        //   const { batchHash, totalChunksCount, endBatchIndex } = batchesMetadata[
+        //     batchKey
+        //   ];
+        //   // We will send these batch of chunks to other peer
+        //   const batchOfChunksIDB = await loadBatchOfChunks(
+        //     batchHash,
+        //     endBatchIndex,
+        //     totalChunksCount
+        //   );
+
+        //   const isBatchExists = await isBatchAlreadyExistOnReceiver(batchHash);
+        //   if (!isBatchExists) {
+        //     const fileSize = fileMetadata["fileSize"];
+        //     setStatus("<h2>File chunks loading in memory and sending...</h2>");
+        //     await sendBatchOfChunks(fileName, batchOfChunksIDB, batchHash);
+        //     await waitForBatchConfirmation(
+        //       fileName,
+        //       batchKey,
+        //       batchHash,
+        //       batchOfChunksIDB,
+        //       endBatchIndex,
+        //       fileSize
+        //     );
+        //     const status =
+        //       endBatchIndex !== fileSize
+        //         ? `<h2>
+        //         ${(endBatchIndex / 1000 / 1000).toFixed(
+        //           2
+        //         )} MB Have Been Send out of ${(fileSize / 1000 / 1000).toFixed(
+        //             2
+        //           )} MB ${fileName} file
+        //          </h2>`
+        //         : `<h2>All File Sended Successfully ${fileName}</h2>`;
+        //     setStatus(status);
+        //     console.log("Batch is sended: ", batchKey, fileName);
+        //   }
       }
-
-      // const currentDcCount = Object.keys(alivaWebRTC.dataChannels).length;
-      // console.log("batchesKeys: ", batchesKeys);
-      // if (currentDcCount < 4) {
-      //   await alivaWebRTC.settingUpDatachannels(400);
-      // } else {
-      //   console.log(`${currentDcCount} data channels already exists`);
-      // }
-      for (let key = 0; key < batchesKeys.length; key++) {
-        const batchKey = batchesKeys[key];
-        const { batchHash, totalChunksCount, endBatchIndex } = batchesMetadata[
-          batchKey
-        ];
-        // We will send these batch of chunks to other peer
-        const batchOfChunksIDB = await loadBatchOfChunks(
-          batchHash,
-          endBatchIndex,
-          totalChunksCount
-        );
-
-        const isBatchExists = await isBatchAlreadyExistOnReceiver(batchHash);
-        if (!isBatchExists) {
-          const fileSize = fileMetadata["fileSize"];
-          setStatus("<h2>File chunks loading in memory and sending...</h2>");
-          await sendBatchOfChunks(fileName, batchOfChunksIDB, batchHash);
-          await waitForBatchConfirmation(
-            fileName,
-            batchKey,
-            batchHash,
-            batchOfChunksIDB,
-            endBatchIndex,
-            fileSize
-          );
-          const status =
-            endBatchIndex !== fileSize
-              ? `<h2>
-              ${(endBatchIndex / 1000 / 1000).toFixed(
-                2
-              )} MB Have Been Send out of ${(fileSize / 1000 / 1000).toFixed(
-                  2
-                )} MB ${fileName} file
-               </h2>`
-              : `<h2>All File Sended Successfully ${fileName}</h2>`;
-          setStatus(status);
-          console.log("Batch is sended: ", batchKey, fileName);
-        }
-      }
-      await allFileSendSignal(fileName);
-      await cleanFilePeerConnection(fileName);
-      resolve(true);
+      // await allFileSendSignal(fileName);
+      // await cleanFilePeerConnection(fileName);
+      // resolve(true);
     } catch (error) {
       reject(error);
     }
