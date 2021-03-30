54df531679d99419514bad2eeda531210e8ef460
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-30 12:03:14 +0500

    Update waitForBatchConfirmation.js

--- src/webrtc/waitForBatchConfirmation/waitForBatchConfirmation.js
+++ src/webrtc/waitForBatchConfirmation/waitForBatchConfirmation.js
@@ -47,9 +47,12 @@ export const waitForBatchConfirmation = (
                 resendChunkObj[chunkKey] = resendChunk;
               }
             }
-            console.log("Resending batch: ", resendChunkObj);
-            console.log("Resending batch hash: ", batchHash);
-            console.log("Resending fileName: ", fileName);
+            console.log(
+              "Resending batch: ",
+              fileName,
+              batchHash,
+              missingChunks
+            );
             await sendBatchOfChunks(fileName, resendChunkObj, batchHash);
             dataChannel.send(batchConfirmationPayload);
             console.log("Confirmation resend: ", fileName);
